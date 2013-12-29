f71d7d2180a5e23f54f67a44b5b29aebb7399ca5
bartek <bartekfr@wp.pl>
2013-12-29 19:07:04 +0100

    migration cont, 3d transform for lab page

--- experiments.php
+++ experiments.php
@@ -2,16 +2,37 @@
 	<div class="content-inner">
 		<h1 class="title"><span>Experiments</span></h1>
 		<ul class="site-list experiments">
-			<li><a href="css-power">Power of CSS3</a></li>
-			<li><a href="file">Local files reading with native drag and drop</a></li>
-			<li><a href="rotator">Gallery rotator</a></li>
-			<li><a href="clock">Canvas Clock</a></li>
-			<li><a href="chart">Chart drawing with canvas</a></li>
-			<li><a href="tab">Tabs</a></li>
-			<li><a href="3dtransform">CSS3 Transformation 3D</a></li>
-			<li><a href="collision">(Very) simple canvas collision detection</a></li>
-			<li><a href="transform-order">Importance of transformation order</a></li>
-			<li><a target="_blank" href="project-pages/dictionary-app">Backbone dictionary app</a></li>
+			<li><a class="link" href="css-power">
+				<span class="name">Power of CSS3</span>
+				<span class="description">CSS3 animation, transforms and a few other cool features</span>
+			</a></li>
+			<li><a class="link" href="file">
+				<span class="name">Local files reading</span>
+				<span class="description">HTML5 File and Drag and Drop APIs</span>
+			</a></li>
+			<li><a class="link" href="rotator">
+				<span class="name">Image slider</span>
+				<span class="description">Image slider with fade and carousel effects</span>
+			<li><a class="link" href="clock">
+				<span class="name">Canvas clock</span>
+				<span class="description">HTML5 Canvas</span>
+			</a></li>
+			<li><a class="link" href="chart">
+				<span class="name">Chart drawing</span>
+				<span class="description">Draw chart based on entered data</span>
+			</a></li>
+			<li><a class="link" href="tab">
+				<span class="name">Tab widget</span>
+				<span class="description">JS widget based on CSS3 transition events</span>
+			</a></li>
+			<li><a class="link" href="collision">
+				<span class="name">Simple collison detection</span>
+				<span class="description">Detecting elements collision on canvas element</span>
+			</a></li>
+			<li><a class="link" href="transform-order">
+				<span class="name">Order of transformation</span>
+				<span class="description">See that order of transformation matters</span>
+			</a></li>
 		</ul>
 	</div>
 <?php	include_once 'inc/footer.html.php';?>
--- file.php
+++ file.php
@@ -1,16 +1,14 @@
 <?php	include_once 'inc/header.html.php';?>
-	<div class="content-inner">
-		<h1 class="title"><span>Local files reading with native drag and drop</span></h1>
-		<div id="dragAndDrop">
-			<p>Drag image in green box over the yellow cointainer and drop it to add it to drop area.<br/>
-			You can also drag and drop your local image or text files but because it is new HTML5 feature result depends on browser.
-			</p>
-			<ul>
-				<li><a draggable="true" href="#"><img width="200" src="img/big-banner1.jpg" /></a></li>
-				<li><a draggable="true" href="#"><img width="200" src="img/big-banner2.jpg" /></a></li>
-				<li><a draggable="true" href="#"><img width="200" src="img/big-banner3.jpg" /></a></li>
-			</ul>
-			<div id="drop" ><h1>Drop image here</h1></div>
-		</div>
+	<h1 class="title"><span>Local files reading with native drag and drop</span></h1>
+	<div id="dragAndDrop">
+		<p>Drag image in green box over the yellow cointainer and drop it to add it to drop area.<br/>
+		You can also drag and drop your local image or text files but because it is new HTML5 feature result depends on browser.
+		</p>
+		<ul>
+			<li><a draggable="true" href="#"><img width="200" src="img/big-banner1.jpg" /></a></li>
+			<li><a draggable="true" href="#"><img width="200" src="img/big-banner2.jpg" /></a></li>
+			<li><a draggable="true" href="#"><img width="200" src="img/big-banner3.jpg" /></a></li>
+		</ul>
+		<div id="drop" ><h1>Drop image here</h1></div>
 	</div>
 <?php	include_once 'inc/footer.html.php';?>
