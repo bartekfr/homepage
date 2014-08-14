06711d0e78c061e80211ce31134c82e5f1c1e45e
bartekfr <bartekfr@wp.pl>
2014-08-14 11:40:00 +0200

    responsive menu fix on iphone

--- inc/header.html.php
+++ inc/header.html.php
@@ -8,7 +8,7 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<title>Bartek Fryzowicz</title>
 		<meta name="description" content="">
-		<meta name="viewport" content="width=device-width">
+ 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="stylesheet" href="css/css.css">
 		<link href='http://fonts.googleapis.com/css?family=Asap:400,700' rel='stylesheet' type='text/css'>
 		<script src="js/vendor/modernizr-2.6.2.min.js"></script>
